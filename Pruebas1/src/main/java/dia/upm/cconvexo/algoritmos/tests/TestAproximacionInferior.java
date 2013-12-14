12cb9692af71701c7eb693e0b70f96e0b109e2d7
ivan <ivan.corrales@gmail.com>
2013-12-14 11:38:14 +0100

    9 Noviembre Algoritmos testeados y funcionales

--- Pruebas1/src/main/java/dia/upm/cconvexo/algoritmos/tests/TestAproximacionInferior.java
+++ Pruebas1/src/main/java/dia/upm/cconvexo/algoritmos/tests/TestAproximacionInferior.java
@@ -3,34 +3,36 @@ package dia.upm.cconvexo.algoritmos.tests;
 import java.util.LinkedList;
 import java.util.List;
 
-import dia.upm.cconvexo.algoritmos.QuickHullNuevo;
+import dia.upm.cconvexo.algoritmos.AproximacionGeneral;
+import dia.upm.cconvexo.algoritmos.AproximacionInferior;
+import dia.upm.cconvexo.algoritmos.Jarvis;
 import dia.upm.cconvexo.gestores.GestorConjuntoConvexo;
 import dia.upm.cconvexo.model.Punto;
 
-public class TestQuickHull extends TestAbstract {
+
+public class TestAproximacionInferior extends TestAbstract {
+
+	protected AproximacionGeneral algoritmo = null;
 	
-	protected QuickHullNuevo algoritmo = null;
-
-	public TestQuickHull(String name) {
+	public TestAproximacionInferior(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
-
+		// TODO Auto-generated constructor stub
 	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp();
-		algoritmo = new QuickHullNuevo();
-		list = new LinkedList<Punto>();
+		algoritmo = new AproximacionInferior();
+		algoritmo.setK_franjas(5);
+		list= new LinkedList<Punto>();
 		initPuntos();
 		initCierreConvexoReal();
+		GestorConjuntoConvexo.getInstancia().borraListaPuntos();
+		GestorConjuntoConvexo.getInstancia().setListaPuntos(list);
 	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
+	
 	protected void tearDown() throws Exception {
 		list = null;
 		algoritmo = null;
@@ -39,11 +41,12 @@ public class TestQuickHull extends TestAbstract {
 	
 	public void test1() throws Exception
 	{
-		GestorConjuntoConvexo.getInstancia().setListaPuntos(list);
 		List<Punto> cierreConvexo = new LinkedList<Punto>();
 		algoritmo.start(0);
-//		compruebaCierreConvexo(cierreConvexo);
-		compruebaCierreConvexoPorAristas(GestorConjuntoConvexo.getInstancia().getConjuntoConvexo());
+		//List listaCC= GestorConjuntoConvexo.getInstancia().getListaPuntos();
+		//compruebaCierreConvexo(listaCC);
+		List listaAristas = GestorConjuntoConvexo.getInstancia().getConjuntoConvexo();
+		compruebaCierreConvexoPorAristas(listaAristas);
 	}
 	
 	public void test2() throws Exception
@@ -57,7 +60,6 @@ public class TestQuickHull extends TestAbstract {
 		//List listaCC= GestorConjuntoConvexo.getInstancia().getListaPuntos();
 		//compruebaCierreConvexo(listaCC);
 		List listaAristas = GestorConjuntoConvexo.getInstancia().getConjuntoConvexo();
-		//compruebaCierreConvexoPorAristas(listaAristas,cierreConvexoReal20);
+		compruebaCierreConvexoPorAristasGenerico(listaAristas,cierreConvexoReal20);
 	}
-	
 }
--- Pruebas1/src/main/java/dia/upm/cconvexo/algoritmos/tests/TestJarvis.java
+++ Pruebas1/src/main/java/dia/upm/cconvexo/algoritmos/tests/TestJarvis.java
@@ -3,34 +3,34 @@ package dia.upm.cconvexo.algoritmos.tests;
 import java.util.LinkedList;
 import java.util.List;
 
-import dia.upm.cconvexo.algoritmos.QuickHullNuevo;
+import dia.upm.cconvexo.algoritmos.BusquedaAristas;
+import dia.upm.cconvexo.algoritmos.Jarvis;
 import dia.upm.cconvexo.gestores.GestorConjuntoConvexo;
 import dia.upm.cconvexo.model.Punto;
 
-public class TestQuickHull extends TestAbstract {
+public class TestJarvis extends TestAbstract {
 	
-	protected QuickHullNuevo algoritmo = null;
+	Jarvis algoritmo = null;
+			
 
-	public TestQuickHull(String name) {
+	public TestJarvis(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
-
+		// TODO Auto-generated constructor stub
 	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp();
-		algoritmo = new QuickHullNuevo();
-		list = new LinkedList<Punto>();
+		algoritmo = new Jarvis();
+		list= new LinkedList<Punto>();
 		initPuntos();
 		initCierreConvexoReal();
+		GestorConjuntoConvexo.getInstancia().borraListaPuntos();
+		GestorConjuntoConvexo.getInstancia().setListaPuntos(list);
 	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
+	
 	protected void tearDown() throws Exception {
 		list = null;
 		algoritmo = null;
@@ -39,11 +39,12 @@ public class TestQuickHull extends TestAbstract {
 	
 	public void test1() throws Exception
 	{
-		GestorConjuntoConvexo.getInstancia().setListaPuntos(list);
 		List<Punto> cierreConvexo = new LinkedList<Punto>();
 		algoritmo.start(0);
-//		compruebaCierreConvexo(cierreConvexo);
-		compruebaCierreConvexoPorAristas(GestorConjuntoConvexo.getInstancia().getConjuntoConvexo());
+		//List listaCC= GestorConjuntoConvexo.getInstancia().getListaPuntos();
+		//compruebaCierreConvexo(listaCC);
+		List listaAristas = GestorConjuntoConvexo.getInstancia().getConjuntoConvexo();
+		compruebaCierreConvexoPorAristas(listaAristas);
 	}
 	
 	public void test2() throws Exception
@@ -57,7 +58,6 @@ public class TestQuickHull extends TestAbstract {
 		//List listaCC= GestorConjuntoConvexo.getInstancia().getListaPuntos();
 		//compruebaCierreConvexo(listaCC);
 		List listaAristas = GestorConjuntoConvexo.getInstancia().getConjuntoConvexo();
-		//compruebaCierreConvexoPorAristas(listaAristas,cierreConvexoReal20);
+		compruebaCierreConvexoPorAristasGenerico(listaAristas,cierreConvexoReal20);
 	}
-	
 }
